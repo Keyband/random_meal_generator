6de95382377910b2fae9ee10f0c9acf576e16e74
keyband <keybpr@gmail.com>
2020-12-19 06:41:13 -0300

    Finally managed to read the json

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:convert' as dartConvert;
 import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
@@ -28,14 +29,20 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  Future<http.Response> fetchAlbum() {
-    return http.get('https://www.themealdb.com/api/json/v1/1/random.php');
-  }
+  Future<Album> futureAlbum;
 
   void _incrementCounter() {
     setState(() {
       _counter++;
+      print('Fetching new food');
+      futureAlbum = fetchAlbum();
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
   }
 
   @override
@@ -51,7 +58,18 @@ class _MyHomePageState extends State<MyHomePage> {
             Text('Nome da comida'),
             RaisedButton(
               child: Text('Gerar comida'),
-              onPressed: () {},
+              onPressed: _incrementCounter,
+            ),
+            FutureBuilder<Album>(
+              future: futureAlbum,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text(snapshot.data.strMeal); //
+                } else if (snapshot.hasError) {
+                  return Text("${snapshot.error}");
+                }
+                return CircularProgressIndicator();
+              },
             )
           ],
         ),
@@ -62,5 +80,32 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Icon(Icons.add),
       ),
     );
+  }
+}
+
+class Album {
+  final String strMeal;
+  // final int id;
+  //final String title;
+
+  Album({this.strMeal});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      strMeal: json['strMeal'],
+    );
+  }
+}
+
+Future<Album> fetchAlbum() async {
+  final response =
+      await http.get('https://www.themealdb.com/api/json/v1/1/random.php');
+
+  if (response.statusCode == 200) {
+    var responseBody = dartConvert.jsonDecode(response.body);
+    print(responseBody['meals'][0]['strMeal'] is String);
+    return Album.fromJson(responseBody['meals'][0]);
+  } else {
+    throw Exception('Failed to load album');
   }
 }
